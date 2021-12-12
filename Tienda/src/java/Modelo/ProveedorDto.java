f4dd8e633bebf677259d1113bb2853c81b88a85b
MaycolCe <95987920+MaycolCe@users.noreply.github.com>
2021-12-11 21:57:44 -0500

    Create ProveedorDto.java

---
+++ Tienda/src/java/Modelo/ProveedorDto.java
@@ -0,0 +1,68 @@
+
+package Modelo;
+
+public class ProveedorDto {
+    
+    private String nit;
+    private String ciudad;
+    private String nombre;
+    private String telefono;
+
+    public ProveedorDto(String nit, String ciudad, String nombre, String telefono) {
+        this.nit = nit;
+        this.ciudad = ciudad;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public ProveedorDto(String nit) {
+        this.nit = nit;
+    }
+    
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public String imprimir(){
+    
+    String mensaje="\nnit: "+getNit()+
+                   "\nciudad: "+getCiudad()+
+                   "\nnombre: "+getNombre()+
+                   "\ntelefono: "+getTelefono();
+    
+    return mensaje;
+    
+    }
+    
+    
+    
+}
